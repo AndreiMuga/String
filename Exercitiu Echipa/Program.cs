645400503757262d35803dd3f2b7906c0438e3b6
AndreiMuga <andrei.muga@gmail.com>
2024-04-16 21:24:07 +0300

    commit

---
+++ Exercitiu Echipa/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        string input = "Hi ^> there << I'm+ telling%%you, you &need% to$ do& your $homeworks. @Hate ^me^ %now% and %thank% me &later.";
+        string output = CleanText(input);
+        Console.WriteLine(output);
+    }
+
+    static string CleanText(string input)
+    {
+        // Define a regular expression pattern to match unwanted characters
+        string pattern = @"[\^<>&+@%$]";
+        
+        // Use Regex.Replace to replace unwanted characters with an empty string
+        string cleanedText = Regex.Replace(input, pattern, "");
+        
+        return cleanedText;
+    }
+}
